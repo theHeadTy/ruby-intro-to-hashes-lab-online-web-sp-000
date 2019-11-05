c3dcda2e73f2d40348c6b5faf73ccde442c2558b
generic-puppet-7755 <tasmith1990@gmail.com>
2019-11-05 05:58:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -23,7 +23,7 @@ end
 def monopoly_with_third_tier
   mon = monopoly_with_second_tier
   mon[:railroads][:rent_in_dollars].update({
-    :one_pieces_owned => 25,
+    :one_piece_owned => 25,
     :two_pieces_owned => 50,
     :three_pieces_owned => 100,
     :four_pieces_owned => 200,
